804eeba5d03c4dc1d2a813c1564f3769fcb8924b
Kyle Farmer <kylefarmer85@gmail.com>
2020-08-22 01:59:48 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,13 +16,12 @@ end
 
 def get_english_meaning(path, emoticon)
   emoticons = load_library(path)
-  result = ''
-  emoticons.each do |key, value|
+  result = emoticons.each do |key, value|
     if emoticon == value[1]
-      result = value[0]
-    binding.pry
+      value[0]
   end
 end
+result
 end
 
 def get_japanese_emoticon(path, emoticon)
