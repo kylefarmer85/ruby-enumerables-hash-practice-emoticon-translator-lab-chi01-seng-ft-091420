19fe915e35e5753976be380836f0b4a85417aa99
Kyle Farmer <kylefarmer85@gmail.com>
2020-08-22 01:37:19 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,17 @@
-# require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
-  # code goes here
+def load_library(path)
+  emoticons = YAML.load_file(path)
+  result = {}
+  emoticons.each do |key, value|
+      result[key.to_s] =
+      {
+      :english => value[0],
+      :japanese => value[1]
+      }
+  end
+  result
 end
 
 def get_japanese_emoticon
